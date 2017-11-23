b4792ba01938d863b4e6cebec47d1aead09b78c9
Ponacko <ponacko@gmail.com>
2017-11-23 21:30:18 +0100

    Add background color to film name in list

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_433419/FilmView.kt
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_433419/FilmView.kt
@@ -1,47 +1,41 @@
 package cz.muni.fi.pv256.movio2.uco_433419
 
 import android.content.Context
-import android.graphics.BitmapFactory
+import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.support.constraint.ConstraintLayout
 import android.support.v7.graphics.Palette
 import android.util.AttributeSet
-import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.film_item.view.*
 
 
-class FilmView(context: Context, attrs : AttributeSet) : ConstraintLayout(context, attrs) {
+class FilmView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        filmImage.layout(0,0,filmImage.measuredWidth, filmImage.measuredHeight)
+        filmImage.layout(0, 0, filmImage.measuredWidth, filmImage.measuredHeight)
         filmName.layout(0, filmImage.measuredHeight - 75,
                 filmImage.measuredWidth, filmImage.measuredHeight)
-        filmRating.layout(filmImage.measuredWidth - 150 , filmImage.measuredHeight - 75,
+        filmRating.layout(filmImage.measuredWidth - 150, filmImage.measuredHeight - 75,
                 filmImage.measuredWidth, filmImage.measuredHeight)
     }
 
-    fun setFilmProperties(film : Film){
+
+    fun setFilmProperties(film: Film) {
         filmName.text = film.original_title
         Picasso.with(context)
                 .load("https://image.tmdb.org/t/p/w500" +
                         film.poster_path)
                 .into(filmImage)
-//        val resId = resources.getIdentifier(film.coverPath, "drawable", context.packageName)
-//        val semitransparentColor = getColorFromResource(resId, 0.75f)
-//        val solidColor = getColorFromResource(resId, 1f)
-//        val transparentColor = getColorFromResource(resId, 0f)
-//        filmName.setBackgroundColor(semitransparentColor)
-        //filmImage.setImageResource(resId)
-
         filmRating.text = film.popularity.toString()
+//        val semitransparentColor = getColorFromResource(bitmap, 0.75f)
+//        val solidColor = getColorFromResource(bitmap, 1f)
+//        val transparentColor = getColorFromResource(bitmap, 0f)
 //        val gradientDrawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
 //                intArrayOf(transparentColor, solidColor))
 //        filmRating.background = gradientDrawable
     }
 
-    private fun getColorFromResource(resId : Int, opacity : Float) : Int{
-        val bitmap = BitmapFactory.decodeResource(resources, resId)
+    private fun getColorFromResource(bitmap: Bitmap, opacity: Float): Int {
         val palette = Palette.from(bitmap).generate()
         val color = palette.getDarkVibrantColor(0x000)
         return Color.argb(Math.round(Color.alpha(color) * opacity),
