67814faa8242078749db0d162d654b7bc69520a6
Ponacko <ponacko@gmail.com>
2017-11-25 17:22:44 +0100

    Update build tools version and fix build errors

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_433419/ListFragment.kt
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_433419/ListFragment.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,9 +26,9 @@ class ListFragment : android.support.v4.app.Fragment() {
 
     private var mListener: OnFragmentInteractionListener? = null
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? =// Inflate the layout for this fragment
-            inflater!!.inflate(R.layout.fragment_list, container, false)
+            inflater.inflate(R.layout.fragment_list, container, false)
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
